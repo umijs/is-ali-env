ce740e4c1b110a13fcca48de3a45b5f934a35e56
zhuyue <fuping.dfp@antfin.com>
2018-06-28 11:51:42 +0800

    refactor: Judge whether it is ali env by visiting a private file

--- index.js
+++ index.js
@@ -1,31 +1,18 @@
-const fetch = require('node-fetch');
-const FormData = require('form-data');
+'use strict';
 
-module.exports = function(opts) {
-  opts = opts || {};
-  return new Promise(function(resolve) {
-    const form = new FormData();
-    form.append('ip', 'myip');
+const superagent = require('superagent');
 
-    fetch('http://ip.taobao.com/service/getIpInfo2.php', {
-      method: 'post',
-      body: form,
-      timeout: opts.timeout || 2000,
-    }).then(function(res) {
-      if (res.status === 200) {
-        return res.json();
-      } else {
-        console.log(res.status);
-        resolve(false);
-      }
-    }).then(function(res) {
-      if (res.data && (res.data.isp_id === '100098' || res.data.isp_id === '100017')) {
-        resolve(true);
-      } else {
-        resolve(false);
-      }
-    }).catch(function(err) {
-      resolve(false);
-    });
+module.exports = function() {
+  return new Promise(resolve => {
+    // try to visit a private png file
+    superagent.get('https://private-alipayobjects.alipay.com/alipay-rmsdeploy-image/rmsportal/FlPJSPwhzfagtBoHKCbu.png')
+      .timeout(2000)
+      .end(err => {
+        if (err) {
+          resolve(false);
+        } else {
+          resolve(true);
+        }
+      });
   });
 };
