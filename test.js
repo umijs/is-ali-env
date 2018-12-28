cc990f1905692b560d41c05d914a3f1ef11a89cc
sorrycc <sorrycc@gmail.com>
2018-12-28 16:11:42 +0800

    update

--- test.js
+++ test.js
@@ -1,5 +1,8 @@
 const isAliEnv = require('./index');
 
+console.log('start');
+const start = new Date();
 isAliEnv().then(function(res) {
   console.log(`is ali env: ${res}`);
+  console.log(`end in ${new Date() - start}`);
 });
